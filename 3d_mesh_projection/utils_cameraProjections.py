7b3bdfdc9ed7d3b86f09b942e5cf59283a55cc3b
yan <song.yan@tuni.fi>
2020-01-29 19:09:45 +0200

    update

--- 3d_mesh_projection/utils_cameraProjections.py
+++ 3d_mesh_projection/utils_cameraProjections.py
@@ -2,6 +2,7 @@ import numpy as np
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt 
 import cv2 
+import math
 
 def loadObj(fname, isMeter=True):
 	vert, vt, tris = [], [], []
@@ -76,7 +77,7 @@ class MeshToIMGProjector():
 
 		return flags
 
-	def projectToPixelsCoord(self, pts, camera='ortho'):
+	def projectToPixelsCoord(self, pts, tris, camera='ortho'):
 		''' from world Cooordinates (x, y, z) to Pixel Coordinates (h, w) 
 			Pixels' coordinates refer to is located in the upper-left corner of the image.
 			Pixel scale, 1 pixel = scale_factor *1 mm , default=1
@@ -85,7 +86,8 @@ class MeshToIMGProjector():
 
 		# projection:  orthogonal, weak-perspective, perspective
 		projPts = self.projection(pts, camera=camera)
-
+		insidePixels, insideBarrys, _, _, _ =  self.rasterization(projPts, tris)
+		projPts = np.concatenate((projPts, insidePixels), axis=0)
 		# From Screen Space to Raster Space, Pixel's coordinates are intergers
 		# 1) Re-map P's coordinates in the range [0, 1], Normalized Device Coordinate
 		P_normalized = np.zeros((num_pts, 2), dtype=np.float32)
@@ -128,20 +130,121 @@ class MeshToIMGProjector():
 
 		return visible_idx
 
+	def sortPtsInClockWise(self, pt0, pt1, pt2, vIdx):
+		pts = np.array([pt0, pt1, pt2])
+		# calculate the angles between verts and the center
+		center = np.mean(pts, 0)
+		vectors = pts - center
+		# angle between the vectors and the x-axis, [-pi , pi]
+		angle00 = math.atan2(vectors[0][1], vectors[0][1])
+		angle01 = math.atan2(vectors[1][1], vectors[1][1])
+		angle02 = math.atan2(vectors[2][1], vectors[2][1])
+		angles = np.array([angle00, angle01, angle02])
+		# sort pts by angles, from +pi to -pi, clock-wise
+		sort_idx = np.flip(np.argsort(angles)) 
+		pts = pts[sort_idx]
+		return pts[0], pts[1], pts[2], vIdx[sort_idx]
 
-	def rasterization(self, pixel_coords, tris, pixel_values, depth_values):
+	def getPixelsInTriangle(self, pt0, pt1, pt2):
+		'''
+		 solved by Edge function, 
+		 	E01(P) = (P.x - pt0.x) * (pt1.y - pt0.y) - (P.y - pt0.y) * (pt1.x - pt0.x)
+		 	E12(P) = (P.x - pt1.x) * (pt2.y - pt1.y) - (P.y - pt1.y) * (pt2.x - pt1.x)
+		 	E20(P) = (P.x - pt2.x) * (pt0.y - pt2.y) - (P.y - pt2.y) * (pt0.x - pt2.x)
+			if all E(P)s are positive (clock-wise). then P lies inside the triangles
+		'''
+		pixels = np.empty((0, 2), dtype=np.int32)
+		barrys = np.empty((0, 3), dtype=np.float32)
 
-		triangle = tis[ii, :]
-		# 1) define an area cover the triangle
+		# find the Bound Box
+		xmin = np.rint(min(pt0[0], pt1[0], pt2[0]))
+		xmax = np.rint(max(pt0[0], pt1[0], pt2[0]))
+		ymin = np.rint(min(pt0[1], pt1[1], pt2[1]))
+		ymax = np.rint(max(pt0[1], pt1[1], pt2[1]))
+		# loop to check if pixel lies inside of the triangle
+		for px in range(xmin, xmax):
+			for py in range(ymin, ymax):
+				# Edge Function
+				E01 = (px - pt0[0])*(pt1[1] - pt0[1]) - (py - pt0[1])*(pt1[0] - pt0[0])
+				E12 = (px - pt1[0])*(pt2[1] - pt1[1]) - (py - pt1[1])*(pt2[0] - pt1[0])
+				E20 = (px - pt2[0])*(pt0[1] - pt2[1]) - (py - pt2[1])*(pt0[0] - pt2[0])
+				# if all edge functions return positive or equal to 0
+				if E01 >=0 and E12 >= 0 and E20 >=0:
+					P = np.array([[px, py]])
+					pixels = np.concatenate((pixels, P), axis=0)
+					# get barraycentric coords
+					bc = self.getBarrycentricCoords(P, pt0, pt1, pt2)
+					barrys = np.concatenate((barrys, bc), axis=0)
 
-		# 2) check the points lie inside the triangles
+		return pixels, barrys
 
-		# 3) calculate the barrycentric coordinates: lambda1, labda2, lambda3 
-		#    to calculate depth or RGB
+	def getBarrycentricCoords(self, p, pt0, pt1, pt2):
+		''' 
+		 Barrycentric Coordinates
+			P = lambda0 * V0 + lambda1 * V1 + lambda2 * V2
+			lambda0, lambda1, lambda2 belong to [0, 1]
+			and lambda0 + lambda1 + lambda2 = 1
+			if lambda = 0, then P lies on the edge
+			
+			tri_area = area_BCP + area_ACP + area_ABP
+		'''
+		tri_area = np.linalg.norm(np.cross((pt1 - pt0), (pt2 - pt0))) 
+		if tri_area == 0: # it is not a triable but a point
+			v , u, w = 1, 0, 0
+		else:
+			# triangle BCP corresponds to pt0
+			tri_BCP = np.linalg.norm(np.cross((p - pt2), (pt1 - pt2))) 
+			v = tri_BCP / tri_area
+			# triangle ACP corresponds to pt1
+			tri_ACP = np.linalg.norm(np.cross((pt2 - pt0), (p - pt0))) 
+			w = tri_ACP / tri_area 
+			# triangle ABP corresponds to pt2
+			u = 1 - v - w
+		return np.array([[v, w, u]])
 
-		# 4) 
+	def rasterization(self, pixel_coords, tris, rgb_values=None, depth_values=None, uv_values=None):
+		'''
+		Rasterization is the process of determining which pixels are inside a triangle
+		'''
+		insidePixels = np.empty(shape=(0, 2), dtype=np.int32)
+		insideBarrys = np.empty(shape=(0, 3), dtype=np.float32)
 
-		return image 
+		RGB   = np.empty(shape=(0, 3), dtype=np.float32)
+		UV    = np.empty(shape=(0, 2), dtype=np.float32)
+		DEPTH = np.empty(shape=(0, 1), dtype=np.float32)
+
+		for ii in range(tris.shape[0]):
+			vIdx = tris[ii, :]   
+			V0 = pixel_coords[vIdx[0], :] 
+			V1 = pixel_coords[vIdx[1], :]
+			V2 = pixel_coords[vIdx[2], :]
+			# sort points in clock-wise
+			V0, V1, V2, vIdx = self.sortPtsInClockWise(V0, V1, V2, vIdx)
+			# get pixels inside the triangles and their barrycentric coordinates
+			pixels, bcCoords = self.getPixelsInTriangle(V0, V1, V2)
+			insidePixels = np.concatenate((insidePixels, pixels), axis=0)
+			insideBarrys = np.concatenate((insideBarrys, bcCoords), axis=0)
+
+			# calculate the rgb values, depth values
+			if rgb_values is not None:
+				pixel_rgb = rgb_values[vIdx, :] # (3,3)
+				for bc in bcCoords:
+					pt_rgb = np.matmul(bc, pixel_rgb)
+					RGB = np.concatenate((RGB, pt_rgb), axis=0)
+
+			if depth_values is not None:
+				pixel_depth = depth_values[vIdx, :]
+				for bc in bcCoords:
+					pt_depth = np.matmul(bc, pixel_depth)
+					DEPTH = np.concatenate(DEPTH, pt_depth, axis=0)
+
+			if uv_values is not None:
+				pixel_uv = vu_values[vIdx, :]
+				for bc in bcCoords:
+					pt_uv = np.matmul(bc, pixel_uv)
+					UV = np.concatenate((UV, pt_uv), axis=0)
+
+		return insidePixels, insideBarrys, RGB, DEPTH, UV
 
 if __name__ == '__main__':
 
@@ -153,23 +256,27 @@ if __name__ == '__main__':
 	pts[:, 2] = pts[:, 2] + object_dist # Assume the camera at the origin
 
 	projector = MeshToIMGProjector(image_size=(224, 224), focal_len=35, camera_dist=object_dist)
-	pixel_coords = projector.projectToPixelsCoord(pts, camera='ortho')
+	pixel_coords = projector.projectToPixelsCoord(pts, tris, camera='ortho')
 
-	depth_values = pts[:, 2]  # z-value as the depth
+	# insidePixels, insideBarrys, _, _, _ =  projector.rasterization(pixel_coords, tris)
 
+	# pixel_coords = np.concatenate((pixel_coords, insidePixels), axis=0)
+
+	depth_values = np.ones((pixel_coords.shape[0],), dtype=np.float32)*255
 	pixel_values = np.ones((pixel_coords.shape[0],), dtype=np.float32)*255
+
 	mask  = projector.projectPixelCoordsToImg(pixel_coords, pixel_values, depth_values)
-	u_map = projector.projectPixelCoordsToImg(pixel_coords, vt[:, 0], depth_values) 
-	v_map = projector.projectPixelCoordsToImg(pixel_coords, vt[:, 1], depth_values)
-	d_map = projector.projectPixelCoordsToImg(pixel_coords, depth_values, depth_values)
+	# u_map = projector.projectPixelCoordsToImg(pixel_coords, vt[:, 0], depth_values) 
+	# v_map = projector.projectPixelCoordsToImg(pixel_coords, vt[:, 1], depth_values)
+	# d_map = projector.projectPixelCoordsToImg(pixel_coords, depth_values, depth_values)
 	
-	visible_idx = projector.checkVisibleByDepth(pixel_coords, depth_values)
+	# visible_idx = projector.checkVisibleByDepth(pixel_coords, depth_values)
 
 	cv2.imwrite('mask.png', mask)
-	cv2.imwrite('u_map.png', np.uint8(u_map*255))
-	cv2.imwrite('v_map.png', np.uint8(v_map*255))
-	cv2.imwrite('d_map.png', d_map)
-	cv2.imwrite('visible_idx.png', visible_idx)
+	# cv2.imwrite('u_map.png', np.uint8(u_map*255))
+	# cv2.imwrite('v_map.png', np.uint8(v_map*255))
+	# cv2.imwrite('d_map.png', d_map)
+	# cv2.imwrite('visible_idx.png', visible_idx)
 
 	# fig = plt.figure()
 	# ax = fig.add_subplot(111)
